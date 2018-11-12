e061e815b8cb16b17a8656ba1a3d057cca5b0e08
Douglas Paz <eu@douglaspaz.com>
2018-11-12 15:58:20 -0300

    Update test_docker

--- test_docker/test/__init__.py
+++ test_docker/test/__init__.py
@@ -3,6 +3,7 @@ import logging
 from time import sleep
 
 from celery_worker_on_demand import CeleryWorkerOnDemand
+from celery_worker_on_demand import UpWorker
 
 from .celery_app import celery_app  # noqa:F401
 from . import tasks  # noqa:F401
@@ -10,18 +11,14 @@ from . import tasks  # noqa:F401
 
 logger = logging.getLogger('test-docker')
 
+docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
-class MyDemand(CeleryWorkerOnDemand):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.docker_client = docker.DockerClient(
-            base_url='unix://var/run/docker.sock',
-        )
 
-    def up_worker(self, queue):
-        container = self.docker_client.containers.run(
+class MyUpWorker(UpWorker):
+    def run(self):
+        container = docker_client.containers.run(
             'docker-test-app:latest',
-            entrypoint=f'celery -A test worker -l INFO -Q {queue}',
+            entrypoint=f'celery -A test worker -l INFO -Q {self.queue.name}',
             environment={
                 'BROKER': 'redis://redis:6379/0',
                 'BACKEND': 'redis://redis:6379/0',
@@ -35,10 +32,11 @@ class MyDemand(CeleryWorkerOnDemand):
             network='docker-test-app',
             detach=True,
         )
-        while container.status != 'running' or \
-                not self.exists_worker_to_queue(queue):
+        while not self.queue.has_worker:
             container.reload()
             logger.debug(f'container.status is: {container.status}')
-            if container.status in ['running']:
-                pass
-            sleep(.25)
+            sleep(1)
+
+
+class MyDemand(CeleryWorkerOnDemand):
+    UpWorker = MyUpWorker
